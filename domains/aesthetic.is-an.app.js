ef38fe37232eac06be419bec45e88cf16618fedb
HM <95879255+HMser@users.noreply.github.com>
2023-10-06 16:03:34 +0530

    Create aesthetic.is-an.app.js

---
+++ domains/aesthetic.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'A Website to request songs from my telegram group',
+  domain: 'is-an.app',
+  subdomain: 'aesthetic',
+  owner: {
+    email: 'hmfamily26@gmail.com',
+    repo: 'https://github.com/hmser/aesthetic'
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com'
+  },
+  proxy: false
+});
